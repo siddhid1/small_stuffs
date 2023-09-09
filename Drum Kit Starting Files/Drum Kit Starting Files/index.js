7c0a73fc07ed18edf4c0b3a634479808460ca8e6
siddhid1 <siddhid1@gmail.com>
2023-09-09 18:26:13 +0530

    cant fig out why it be not working

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -4,14 +4,43 @@
 // alert("aaji koi daba dala");
 // }
 
-
 let numberOfDrumsButton = document.querySelectorAll(".drum").length;
 
-for(let i = 0 ; i < numberOfDrumsButton ; i++)
-{
-    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-        alert("aaji koi daba dala");
-        });
+for (let i = 0; i < numberOfDrumsButton; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    let buttonInnerHTML = this.InnerHTML;
+    switch (buttonInnerHTML) {
+      case "w":
+        let tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "a":
+        let tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "s":
+        let tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "d":
+        let tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      case "j":
+        let snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+      case "k":
+        let crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+      case "l":
+        let kick = new Audio("sounds/kick.mp3");
+        kick.play();
+        break;
 
+      default:
+        console.log(buttonInnerHTML);
+    }
+  });
 }
-
